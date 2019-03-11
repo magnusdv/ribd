4b756d1ddb212dfa6fa8b38d476603a16cf7a3e0
magnusdv <magnusdv@medisin.uio.no>
2019-03-11 20:48:44 +0100

    New functions `inbreedingX()` and kinship2_inbreedingX()`

--- R/external_relatedness.R
+++ R/external_relatedness.R
@@ -3,17 +3,21 @@
 #' Wrappers for functions in other packages or external programs, computing
 #' various relatedness coefficients.
 #'
-#' Both `kinship2_inbreeding` and `kinship2_kinship` are wrappers of
-#' [kinship2::kinship()]. Similarly, `jacquard` wraps
-#' [identity::identity.coefs()], which is an R interface for the C program
-#' `IdCoefs` written by Mark Abney (2009). The `identity.coefs()` function
-#' sometimes causes R to crash, hence I have provided an alternative wrapper,
-#' `jacquard2`, which executes an external call to the original C program
-#' `IdCoefs` (version 2.1.1). For this to work, `IdCoefs` must be installed on
-#' the computer (see link in the References section below) and the executable
-#' placed in a folder included in the PATH variable. The `jacquard2` wrapper
-#' works by writing the necessary files to disk and calling `IdCoefs` via
-#' [system()].
+#' `kinship2_inbreeding()` and `kinship2_kinship()` are wrappers of
+#' [kinship2::kinship(..., chrtype = "autosome")].
+#'
+#' Similarly ,`kinship2_inbreedingX()` and `kinship2_kinshipX()` wrap
+#' [kinship2::kinship(..., chrtype = "x")].
+#'
+#' `jacquard()` wraps [identity::identity.coefs()], which is an R interface for
+#' the C program `IdCoefs` written by Mark Abney (2009). The `identity.coefs()`
+#' function sometimes causes R to crash, hence I have provided an alternative
+#' wrapper, `jacquard2`, which executes an external call to the original C
+#' program `IdCoefs` (version 2.1.1). For this to work, `IdCoefs` must be
+#' installed on the computer (see link in the References section below) and the
+#' executable placed in a folder included in the PATH variable. The
+#' `jacquard2()` wrapper works by writing the necessary files to disk and
+#' calling `IdCoefs` via [system()].
 #'
 #' @param x A pedigree, in the form of a [`pedtools::ped`] object.
 #' @param ids A integer vector of length 2.
@@ -21,14 +25,14 @@
 #'   printed.
 #' @param cleanup A logical: If TRUE, the pedfile and sample file created for
 #'   the IdCoefs run are deleted automatically.
-#' @return For `kinship2_inbreeding`, a named numerical vector with the
-#'   inbreeding coefficients and ID labels as names.
+#' @return For `kinship2_inbreeding()` and `kinship2_inbreedingX()`, a named
+#'   numerical vector with the inbreeding coefficients and ID labels as names.
 #'
-#'   For `kinship2_kinship`, either a single numeric (if `ids` is a pair of
-#'   pedigree members) or the whole kinship matrix, with the ID labels as
-#'   dimnames.
+#'   For `kinship2_kinship()` and `kinship2_kinshipX()`, either a single numeric
+#'   (if `ids` is a pair of pedigree members) or the whole kinship matrix, with
+#'   the ID labels as dimnames.
 #'
-#'   For `jaquard` and `jaquard2`, a numerical vector of length 9 (in the
+#'   For `jaquard()` and `jaquard2()`, a numerical vector of length 9 (in the
 #'   standard order of Jacquard's condensed identity coefficients).
 #' @author Magnus Dehli Vigeland
 #' @seealso [kinship2::kinship()], [identity::identity.coefs()]
@@ -38,49 +42,24 @@
 #'   <http://home.uchicago.edu/~abney/abney_web/Software.html>
 #'
 #' @examples
-#' library(pedtools)
+#' # A random pedigree with 2 founders and 5 matings
+#' p = pedtools::randomPed(g = 5, founders=2, seed=111)
 #'
-#' # Offspring of first cousins
-#' x = cousinPed(1, child = TRUE)
-#' inb = kinship2_inbreeding(x)
-#' stopifnot(inb[9] == 1/16)
+#' # Check that ribd agrees with kinship2 package
+#' kinship_k2 = kinship2_kinship(p)
+#' kinship_ribd = kinship(p)
+#' identical(kinship_k2, kinship_ribd)
 #'
-#' # if ID labels are not 1:9, care must be taken in extracting correct elements.
-#' y = relabel(x, 9:1)
-#' y
-#' inb = kinship2_inbreeding(y)
+#' # Check on X also
+#' kinshipX_k2 = kinship2_kinshipX(p)
+#' kinshipX_ribd = kinshipX(p)
+#' identical(kinshipX_k2, kinshipX_ribd)
 #'
-#' # The child's Id is now '1'.
-#' child = leaves(y)
-#'
-#' # Note the difference.
-#' inb[1]   # wrong
-#' inb['1'] # correct
-#'
-#' # The inbreeding coeff of the child equals the kinship coeff of parents
-#' par = parents(y, child)
-#' kin = kinship2_kinship(y, par)
-#' stopifnot(inb[child] == kin)
-#'
-#' @name relatednessCoeff
+#' @name external_coefs
 NULL
 
-#' @rdname relatednessCoeff
+#' @rdname external_coefs
 #' @importFrom kinship2 kinship
-#' @export
-kinship2_inbreeding = function(x) {
-  kin.matrix = kinship2_kinship(x)
-
-  inb = numeric(pedsize(x))
-  names(inb) = labels(x)
-
-  nonf_int = nonfounders(x, internal = TRUE)
-  inb[nonf_int] = kin.matrix[cbind(x$FIDX, x$MIDX)] # founders -> kin[0,0] -> excluded
-
-  inb
-}
-
-#' @rdname relatednessCoeff
 #' @export
 kinship2_kinship = function(x, ids = NULL) {
   if(!is.ped(x)) stop2("Input is not a `ped` object")
@@ -99,7 +78,8 @@ kinship2_kinship = function(x, ids = NULL) {
   kin.matrix[as.character(ids[1]), as.character(ids[2])]
 }
 
-#' @rdname relatednessCoeff
+#' @rdname external_coefs
+#' @importFrom kinship2 kinship
 #' @export
 kinship2_kinshipX = function(x, ids = NULL) {
   if(!is.ped(x)) stop2("Input is not a `ped` object")
@@ -118,7 +98,37 @@ kinship2_kinshipX = function(x, ids = NULL) {
   kin.matrix[as.character(ids[1]), as.character(ids[2])]
 }
 
+#' @rdname external_coefs
+#' @importFrom kinship2 kinship
+#' @export
+kinship2_inbreeding = function(x) {
+  kin.matrix = kinship2_kinship(x)
+
+  inb = numeric(pedsize(x))
+  names(inb) = labels(x)
+
+  nonf_int = nonfounders(x, internal = TRUE)
+  inb[nonf_int] = kin.matrix[cbind(x$FIDX, x$MIDX)] # founders -> kin[0,0] -> excluded
+
+  inb
+}
+
 #' @rdname relatednessCoeff
+#' @importFrom kinship2 kinship
+#' @export
+kinship2_inbreedingX = function(x) {
+  kin.matrix = kinship2_kinshipX(x)
+
+  inb = numeric(pedsize(x))
+  names(inb) = labels(x)
+
+  nonf_int = nonfounders(x, internal = TRUE)
+  inb[nonf_int] = kin.matrix[cbind(x$FIDX, x$MIDX)] # founders -> kin[0,0] -> excluded
+
+  inb
+}
+
+#' @rdname external_coefs
 #' @export
 jacquard = function(x, ids) {
   if (!requireNamespace("identity", quietly = TRUE))
@@ -138,7 +148,7 @@ jacquard = function(x, ids) {
 }
 
 
-#' @rdname relatednessCoeff
+#' @rdname external_coefs
 #' @importFrom utils read.table write.table
 #' @export
 jacquard2 = function(x, ids, verbose = FALSE, cleanup = TRUE) {
--- R/inbreeding.R
+++ R/inbreeding.R
@@ -1,14 +1,17 @@
 #' Inbreeding coefficients
 #'
-#' Compute the inbreeding coefficients of all members of a pedigree. This is a
-#' wrapper of [kinship()] which does the main work. The founders may be
-#' inbred; see [pedtools::founderInbreeding()] for how to set this up.
+#' Compute the inbreeding coefficients of all members of a pedigree. These are
+#' wrappers of [kinship()] and [kinshipX()] which do the main work. The founders
+#' may be inbred; see [pedtools::founderInbreeding()] for how to set this up.
 #'
-#' The inbreeding coefficient of a pedigree member is defined as the probability
-#' that, at a random autosomal locus, the two alleles carried by the member are
-#' identical by descent relative to the pedigree. It follows from the definition
-#' that the inbreeding coefficient of a member equals the kinship coefficient of
-#' the parents.
+#' The autosomal inbreeding coefficient of a pedigree member is defined as the
+#' probability that, at a random autosomal locus, the two alleles carried by the
+#' member are identical by descent relative to the pedigree. It follows from the
+#' definition that the inbreeding coefficient of a member equals the kinship
+#' coefficient of the parents.
+#'
+#' The X chromosomal inbreeding coefficient of an female member is defined
+#' similarly to the autosomal case above. For males is it always 1.
 #'
 #' @param x A pedigree, in the form of a [`pedtools::ped`] object.
 #'
@@ -24,6 +27,13 @@
 #' founderInbreeding(x, 1) = 1
 #' inbreeding(x)
 #'
+#' # The X inbreeding coefficients depend on the gender distribution
+#' x.pat = halfCousinPed(0, child = TRUE) # child of paternal half sibs
+#' inbreedingX(x.pat) # all zero
+#'
+#' x.mat = swapSex(x.pat, 1) # change to maternal half sibs
+#' inbreedingX(x.mat) # child now has f_X = 1/4
+#'
 #' @export
 inbreeding = function(x) {
   kin.matrix = kinship(x)
@@ -34,6 +44,24 @@ inbreeding = function(x) {
 
   # Fill in founder inbreeding
   inb[founders(x)] = founderInbreeding(x)
+
+  # Non-founders
+  inb[nonfounders(x)] = kin.matrix[cbind(x$FIDX, x$MIDX)] # founders -> kin[0,0] -> excluded
+
+  inb
+}
+
+#' @rdname inbreeding
+#' @export
+inbreedingX = function(x) {
+  kin.matrix = kinshipX(x)
+
+  # Initialize result vector
+  inb = numeric(pedsize(x))
+  names(inb) = labels(x)
+
+  # Fill in founder inbreeding
+  # inb[founders(x)] = founderInbreeding(x)
 
   # Non-founders
   inb[nonfounders(x)] = kin.matrix[cbind(x$FIDX, x$MIDX)] # founders -> kin[0,0] -> excluded
