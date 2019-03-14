91cf84e1af4869ce0111b2c61b89b8a61b1117c0
magnusdv <magnusdv@medisin.uio.no>
2019-03-14 14:53:56 +0100

    Update kappa tests

--- tests/testthat/test-kappa.R
+++ tests/testthat/test-kappa.R
@@ -2,8 +2,8 @@ context("Kinship")
 
 test_that("kappa gives message and returns NA,NA,NA if inbreeding", {
   x = fullSibMating(1)
-  expect_message(kappa(x, 5:6), "Inbred individuals: 5, 6")
-  expect_identical(kappa(x, 5:6), rep(NA_real_, 3))
+  expect_message(kappaIbd(x, 5:6, verbose=T), "kappas involving inbred individuals are undefined")
+  expect_identical(kappaIbd(x, 5:6), rep(NA_real_, 3))
 })
 
 test_that("kappa allows for rounding errors in the detection of inbreeding", {
@@ -20,7 +20,7 @@ test_that("kappa allows for rounding errors in the detection of inbreeding", {
 
   # Computed
   founderInbreeding(x, 1:2) = c(f1, f2)
-  kappa_ribd = kappa(x, ids = 3:4)
+  kappa_ribd = kappaIbd(x, ids = 3:4)
 
   # Results are not identical (due to rounding), but should be *equal*
   expect_equal(kappa_ribd, kappa_exact)
