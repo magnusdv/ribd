d181f752193905b731de3209086fcbc4b3a674ea
magnusdv <magnusdv@medisin.uio.no>
2019-04-15 16:50:30 +0200

    Major rewrite of `twoLocusKinship()`, refactor code

--- R/twoLocusKinship.R
+++ R/twoLocusKinship.R
@@ -70,16 +70,16 @@ twoLocusKinship = function(x, ids, rho, recombinants = NULL, verbose = FALSE, de
   if(!has_parents_before_children(x))
     x = parents_before_children(x)
 
-  ids_int = internalID(x, ids)
-
   # Convert recombination conditions from logical to list(r = , nr = )
   recombList = if(is.null(recombinants)) NULL else list(nr = ids[!recombinants], r = ids[recombinants])
+
+  ids_int = internalID(x, ids)
 
   if(length(ids) == 2) {
     mem = initialiseTwoLocusMemo(x, rho = rho, recomb = recombList)
 
-    A = C = c(ids_int[1], 0)
-    B = D = c(ids_int[2], 0)
+    A = C = c(ids_int[1], -1) # using negative numbers to enforce independent gametes
+    B = D = c(ids_int[2], -2)
     phi11 = twoLocKin(A, B, C, D, mem, indent = ifelse(debug, 0, NA))
 
     # Print info
@@ -90,12 +90,13 @@ twoLocusKinship = function(x, ids, rho, recombinants = NULL, verbose = FALSE, de
                        Calls = {mem$i}
                        Lookups = {mem$ilook}
                        Recursions: {mem$irec}
-                          eq. 7: {mem$eq7}
-                          eq. 8: {mem$eq8}
-                          eq. 9a: {mem$eq9a}
-                          eq. 9b: {mem$eq9b}
-                          eq. 10: {mem$eq10}
-                          eq. 11: {mem$eq11}
+                          eq. 7  : {mem$eq7}
+                          eq. 8  : {mem$eq8}
+                          eq. 9a : {mem$eq9a}
+                          eq. 9b : {mem$eq9b}
+                          eq. 10 : {mem$eq10}
+                          eq. 11a: {mem$eq11a}
+                          eq. 11b: {mem$eq11b}
                        Total time used: {totsecs} seconds"))
     }
 
@@ -114,8 +115,8 @@ twoLocusKinship = function(x, ids, rho, recombinants = NULL, verbose = FALSE, de
     mem$rho = r
 
     unlist(lapply(pairs, function(p) {
-      A = C = c(p[1], 0)
-      B = D = c(p[2], 0)
+      A = C = c(p[1], -1)
+      B = D = c(p[2], -2)
       twoLocKin(A, B, C, D, mem, indent = ifelse(debug, 0, NA))
     }))
   })
@@ -151,150 +152,46 @@ twoLocKin = function(A, B, C, D, mem, indent = 0) {
   A = plist[[1]]; B = plist[[2]]; C = plist[[3]]; D = plist[[4]]
   a = A[1]; b = B[1]; c = C[1]; d = D[1]
 
-  FF = mem$FIDX[a]
-  MM = mem$MIDX[a]
   isFou = mem$isFounder[a]
   ANC = mem$anc
-  k1 = mem$k1
-  rho = mem$rho
-  forceNonRec = a %in% mem$recomb$nr
-  forceRec = a %in% mem$recomb$r
-
-  ### The recursions (eqs. 7-11 in Thompson (1988))
 
   k2_recurse = function(A,B,C,D) {
     mem$irec = mem$irec + 1
     a = A[1]; b = B[1]; c = C[1]; d = D[1]
 
-    if(a > b && a > c) { # eq. 7
-      mem$eq7 = mem$eq7 + 1
+    if(identical(A, B))
+      stop2("Identical gametes at first locus; not implemented. (This should not occur in a two-locus kinship computation.)")
 
-      if(isFou)
-        0
-      else {
-        t1 = twoLocKin(c(FF, a), B, C, D, mem, indent = indent + 2)
-        t2 = twoLocKin(c(MM, a), B, C, D, mem, indent = indent + 2)
-        0.5 * (t1 + t2)
-      }
+    if(a > b && a > c) { # eq. 7
+      recurse_eq7(A,B,C,D,mem,indent)
     }
     else if(a == b && a > c) { # eq. 8
-      mem$eq8 = mem$eq8 + 1
-
-      if(isFou)
-        0.5 * k1[[c,d]]
-      else {
-        if(A[2] == B[2]) stop("this shouldn't happen")
-        0.5 * (k1[[c,d]] + twoLocKin(c(FF, a), c(MM, a), C, D, mem, indent = indent + 2))
-      }
+      recurse_eq8(A,B,C,D,mem,indent)
     }
-    else if(a > b && a == c) { # eqs. 9a & 9b
-      if(A[2] == C[2]) { # eq. 9a
-        mem$eq9a = mem$eq9a + 1
-
-        if(isFou)
-          0
-        else if(forceNonRec) {
-          t1 = twoLocKin(c(FF, a), B, c(FF, a), D, mem, indent = indent + 2)
-          t2 = twoLocKin(c(MM, a), B, c(MM, a), D, mem, indent = indent + 2)
-
-          0.5 * (1-rho) * (t1 + t2)
-        }
-        else if(forceRec) {
-          t3 = twoLocKin(c(FF, a), B, c(MM, a), D, mem, indent = indent + 2)
-          t4 = twoLocKin(c(MM, a), B, c(FF, a), D, mem, indent = indent + 2)
-
-          0.5 * rho * (t3 + t4)
-        }
-        else {
-          t1 = twoLocKin(c(FF, a), B, c(FF, a), D, mem, indent = indent + 2)
-          t2 = twoLocKin(c(MM, a), B, c(MM, a), D, mem, indent = indent + 2)
-          t3 = twoLocKin(c(FF, a), B, c(MM, a), D, mem, indent = indent + 2)
-          t4 = twoLocKin(c(MM, a), B, c(FF, a), D, mem, indent = indent + 2)
-
-          0.5 * ((1-rho)*(t1 + t2) + rho*(t3 + t4))
-        }
-      }
-      else {  # eq. 9b
-        mem$eq9b = mem$eq9b + 1
-        if(isFou)
-          0
-        else {
-          t1 = twoLocKin(c(FF, a), B, c(FF, a), D, mem, indent = indent + 2)
-          t2 = twoLocKin(c(MM, a), B, c(MM, a), D, mem, indent = indent + 2)
-          t3 = twoLocKin(c(FF, a), B, c(MM, a), D, mem, indent = indent + 2)
-          t4 = twoLocKin(c(MM, a), B, c(FF, a), D, mem, indent = indent + 2)
-
-          0.25 * (t1 + t2 + t3 + t4)
-        }
-      }
+    else if(a > b && a == c) { #eq. 9a or 9b
+      if(A[2] == C[2])
+        recurse_eq9a(A,B,C,D,mem,indent)
+      else
+        recurse_eq9b(A,B,C,D,mem,indent)
     }
     else if(a == b && a == c && a > d) { # eq. 10
-      mem$eq10 = mem$eq10 + 1
-
-      if(isFou)
-        0
-      else {
-        t3 = twoLocKin(c(FF, a), c(MM, a), c(FF, a), D, mem, indent = indent + 2)
-        t4 = twoLocKin(c(FF, a), c(MM, a), c(MM, a), D, mem, indent = indent + 2)
-        s = 0.25 * (k1[[FF, d]] + k1[[MM, d]] + t3 + t4)
-
-        if(forceNonRec)
-          (1-rho) * s
-        else if(forceRec)
-          rho * s
-        else
-          s
-      }
+      recurse_eq10(A,B,C,D,mem,indent)
     }
-    else if((a == b && a == c && a == d)) { # eq. 11
-      mem$eq11 = mem$eq11 + 1
-      R = .5*(rho^2 + (1-rho)^2)
+    else if((a == b && a == c && a == d)) {
+      # This case needs further branching!
 
-      # This case needs further branching! A bit vague in the EAT-paper.
-      type = sum(c(A[2], B[2]) %in% c(C[2], D[2]))
+      if(A[2] == C[2] && B[2] == D[2]) # k2(J(A1, A2), L(A1, A2))
+        recurse_eq11a(A,B,C,D,mem,indent)
 
-      if(type == 2) { # this is eq. 11 in the paper
-        if(isFou) {
-          if(forceNonRec && forceRec) 0
-          else if(forceNonRec) .5 * (1-rho)^2
-          else if(forceRec) .5 * rho^2
-          else R
-        }
-        else {
-          if(forceNonRec && forceRec) rho*(1-rho)*k1[[MM, FF]] # without the factor two (either R-NR or NR-R)
-          else {
-            t4 = twoLocKin(c(MM, a), c(FF, a), c(MM, a), c(FF, a), mem, indent = indent + 2)
-            if(forceNonRec) .5 * (1-rho)^2 * (1 + t4)
-            else if(forceRec) .5 * rho^2 * (1 + t4)
-            else 2*rho*(1-rho)*k1[[MM, FF]] + R*(1 + t4)
-          }
-        }
-      }
-      else if(type == 1) { # a.k.a. k2(J(A1, A2), L(A1, A3))
-        if(forceNonRec | forceRec)
-          stop2("Special case: Not implemented.")
-        if(isFou)
-          1/4
-        else {
-          t1 = twoLocKin(c(FF, a), B, c(FF, a), D, mem, indent = indent + 2)
-          t2 = twoLocKin(c(MM, a), B, c(MM, a), D, mem, indent = indent + 2)
-          t3 = twoLocKin(c(FF, a), B, c(MM, a), D, mem, indent = indent + 2)
-          t4 = twoLocKin(c(MM, a), B, c(FF, a), D, mem, indent = indent + 2)
-
-          0.5 * ((1-rho)*(t1 + t2) + rho*(t3 + t4))
-        }
-      }
-      else if(type == 0) { # a.k.a. k2(J(A1, A2), L(A3, A4))
-        stop2("Special case k2(J(A1, A2), L(A3, A4)): Not implemented yet")
-      }
+      else  # k2(J(A1, A2), L(A1, A3)) or k2(J(A1, A2), L(A3, A4))
+        recurse_eq11b(A,B,C,D,mem,indent)
     }
   }
+
 
   # If no common ancestors, return 0
-  if(!(ANC[a,b] && ANC[c,d])) {
-    if(!is.na(indent)) message(strrep(" ", indent), 0)
-    return(0)
-  }
+  if(!(ANC[a,b] && ANC[c,d]))
+    return(printAndReturn(0, indent))
 
   # Lookup in array; compute if necessary.
   mem$ilook = mem$ilook + 1
@@ -302,9 +199,192 @@ twoLocKin = function(A, B, C, D, mem, indent = 0) {
   if(is.null(res))
     res = mem$k2[[toString(plist)]] = k2_recurse(A, B, C, D)
 
-  if(!is.na(indent)) message(strrep(" ", indent), res)
+  printAndReturn(res, indent)
+}
+
+
+printMess = function(plist, indent) {
+  if(is.na(indent)) return()
+  pp = sapply(plist, function(p) paste(p, collapse=":"))
+  message(sprintf("%sJ(%s, %s), L(%s, %s)",
+                  strrep(" ", indent), pp[1], pp[2], pp[3], pp[4]))
+}
+
+printAndReturn = function(res, indent) {
+  if(!is.na(indent))
+    message(strrep(" ", indent), res)
   res
 }
+
+
+### The recursions (eqs. 7-11 in Thompson (1988) + additional cases in Weeks&Lange)
+
+recurse_eq7 = function(A,B,C,D,mem,indent) {
+  mem$eq7 = mem$eq7 + 1
+  a = A[1]
+  if(mem$isFounder[a])
+    return(0, indent)
+
+  FF = mem$FIDX[a]
+  MM = mem$MIDX[a]
+
+  t1 = twoLocKin(c(FF, a), B, C, D, mem, indent = indent + 2)
+  t2 = twoLocKin(c(MM, a), B, C, D, mem, indent = indent + 2)
+  0.5 * (t1 + t2)
+}
+
+recurse_eq8 = function(A,B,C,D,mem,indent) {
+  mem$eq8 = mem$eq8 + 1
+  a = A[1]
+  phi_cd = mem$k1[[C[1], D[1]]] # kinship of c and d
+
+  if(mem$isFounder[a])
+    return(0.5 *  phi_cd)
+
+  FF = mem$FIDX[a]
+  MM = mem$MIDX[a]
+  0.5 * (phi_cd + twoLocKin(c(FF, a), c(MM, a), C, D, mem, indent = indent + 2))
+}
+
+recurse_eq9a = function(A,B,C,D,mem,indent) {
+  mem$eq9a = mem$eq9a + 1
+  a = A[1]
+  if(mem$isFounder[a])
+    return(0)
+
+  FF = mem$FIDX[a]
+  MM = mem$MIDX[a]
+  rho = mem$rho
+
+  # Terms used in various formulas
+  t1 = function() twoLocKin(c(FF, a), B, c(FF, a), D, mem, indent = indent + 2)
+  t2 = function() twoLocKin(c(MM, a), B, c(MM, a), D, mem, indent = indent + 2)
+  t3 = function() twoLocKin(c(FF, a), B, c(MM, a), D, mem, indent = indent + 2)
+  t4 = function() twoLocKin(c(MM, a), B, c(FF, a), D, mem, indent = indent + 2)
+
+  if(mem$nonrecomb[a])              # force non-rec
+    res = 0.5 * (1-rho) * (t1() + t2())
+  else if(mem$recomb[a])          # force rec
+    res = 0.5 * rho * (t3() + t4())
+  else                                  # normal recursion
+    res = 0.5 * ((1-rho) * (t1() + t2()) + rho * (t3() + t4()))
+
+  res
+}
+
+recurse_eq9b = function(A,B,C,D,mem,indent) {
+  mem$eq9b = mem$eq9b + 1
+  a = A[1]
+
+  if(mem$isFounder[a])
+    return(0)
+
+  FF = mem$FIDX[a]
+  MM = mem$MIDX[a]
+
+  t1 = twoLocKin(c(FF, a), B, c(FF, a), D, mem, indent = indent + 2)
+  t2 = twoLocKin(c(MM, a), B, c(MM, a), D, mem, indent = indent + 2)
+  t3 = twoLocKin(c(FF, a), B, c(MM, a), D, mem, indent = indent + 2)
+  t4 = twoLocKin(c(MM, a), B, c(FF, a), D, mem, indent = indent + 2)
+
+  0.25 * (t1 + t2 + t3 + t4)
+}
+
+recurse_eq10 = function(A,B,C,D,mem,indent) {
+  mem$eq10 = mem$eq10 + 1
+  a = A[1]
+
+  if(mem$isFounder[a])
+    return(0)
+
+  FF = mem$FIDX[a]
+  MM = mem$MIDX[a]
+  k1 = mem$k1
+  rho = mem$rho
+  d = D[1]
+
+  t3 = twoLocKin(c(FF, a), c(MM, a), c(FF, a), D, mem, indent = indent + 2)
+  t4 = twoLocKin(c(FF, a), c(MM, a), c(MM, a), D, mem, indent = indent + 2)
+
+  if(A[2] == C[2]) { # EAT eq 10
+    s = 0.25 * (k1[[FF, d]] + k1[[MM, d]] + t3 + t4)
+
+    if(mem$nonrecomb[a])
+      res = (1-rho) * s
+    else if(mem$recomb[a])
+      res = rho * s
+    else
+      res = s
+  }
+  else {
+    if(mem$nonrecomb[a] || mem$recomb[a])
+      stop2("eq 10b conditional: Not implemented")
+    res = 0.25 * (2*k1[[a, d]] + t3 + t4)
+  }
+
+  res
+}
+
+
+recurse_eq11a = function(A,B,C,D,mem,indent) { # Case k2(A1,A2; A1,A2): Eq. 11 in EAT
+  mem$eq11a = mem$eq11a + 1
+  a = A[1]
+  forceNonRec = mem$nonrecomb[a]
+  forceRec = mem$recomb[a]
+  rho = mem$rho
+  R = .5 * ((1-rho)^2 + rho^2)
+
+  if(mem$isFounder[a]) {
+    if(forceNonRec && forceRec)
+      res = 0
+    else if(forceNonRec)
+      res = .5 * (1-rho)^2
+    else if(forceRec)
+      res = .5 * rho^2
+    else
+      res = R
+
+    return(res)
+  }
+
+  FF = mem$FIDX[a]
+  MM = mem$MIDX[a]
+  k1 = mem$k1
+
+  if(forceNonRec && forceRec) {
+    res = rho*(1-rho)*k1[[MM, FF]] # without the factor two (either R-NR or NR-R)
+  } else {
+    t4 = twoLocKin(c(MM, a), c(FF, a), c(MM, a), c(FF, a), mem, indent = indent + 2)
+    if(forceNonRec)
+      res = .5 * (1-rho)^2 * (1 + t4)
+    else if(forceRec)
+      res = .5 * rho^2 * (1 + t4)
+    else
+      res = 2*rho*(1-rho)*k1[[MM, FF]] + R*(1 + t4)
+    }
+
+  res
+}
+
+
+recurse_eq11b = function(A,B,C,D,mem,indent) { # k2(A1,A2; A1,A3)
+  # Recursion formula given by Weeks&Lange
+  mem$eq11b = mem$eq11b + 1
+  a = A[1]
+  if(mem$nonrecomb[a] || mem$recomb[a])
+    stop2("11b) conditional: Not implemented")
+
+  if(mem$isFounder[a])
+    return(1/4)
+
+  FF = mem$FIDX[a]
+  MM = mem$MIDX[a]
+  k1 = mem$k1
+
+  parents2L = twoLocKin(c(FF, a), c(MM, a), c(FF, a), c(MM, a), mem, indent = indent + 2)
+  1/4 + 1/2 * k1[[FF, MM]] + 1/4 * parents2L
+}
+
 
 sortPairs = function(A,B,C,D) {
   # Sort: a >= b,c,d; c >= d; if(a==c) then b>=d
@@ -317,15 +397,17 @@ sortPairs = function(A,B,C,D) {
   if(plist[[1]][1] < plist[[3]][1] ||
      (plist[[1]][1] == plist[[3]][1] && plist[[2]][1] < plist[[4]][1]))
     plist[] = plist[c(3,4,1,2)]
+
+  # ensure segregation indicators are sorted appropriately
+  A = plist[[1]]; B = plist[[2]]; C = plist[[3]]; D = plist[[4]]
+  if(A[1]==B[1] && A[1] == C[1] && A[1] == D[1]) {
+    if(A[2] != C[2] && A[2] == D[2])
+      plist[3:4] = plist[4:3]
+  }
+
   plist
 }
 
-printMess = function(plist, indent) {
-  if(is.na(indent)) return()
-  pp = sapply(plist, function(p) paste(p, collapse="-"))
-  message(sprintf("%sA = %s, B = %s, C = %s, D = %s",
-          strrep(" ", indent), pp[1], pp[2], pp[3], pp[4]))
-}
 
 initialiseTwoLocusMemo = function(ped, rho, recomb = NULL, chromType = "autosomal") {
   # Create memory storage
@@ -340,9 +422,11 @@ initialiseTwoLocusMemo = function(ped, rho, recomb = NULL, chromType = "autosoma
   mem$rho = rho
 
   # Conditions on recombinant/non-recombinant gametes
-  if(is.null(recomb))
-    recomb = list(r = NULL, nr = NULL)
-  mem$recomb = recomb
+  mem$recomb = mem$nonrecomb = rep(FALSE, pedsize(ped))
+  if(is.null(recomb)) {
+    mem$recomb[internalID(ped, recomb$r)] = TRUE
+    mem$nonrecomb[internalID(ped, recomb$nr)] = TRUE
+  }
 
   # Logical matrix showing who has a common ancestor within the pedigree.
   mem$anc = has_common_ancestor(ped)
@@ -361,7 +445,7 @@ initialiseTwoLocusMemo = function(ped, rho, recomb = NULL, chromType = "autosoma
 
   # Counters
   mem$i = mem$ilook = mem$irec = 0
-  mem$eq7 = mem$eq8 = mem$eq9a = mem$eq9b = mem$eq10 = mem$eq11 = 0
+  mem$eq7 = mem$eq8 = mem$eq9a = mem$eq9b = mem$eq10 = mem$eq11a = mem$eq11b = 0
 
   # Start time
   mem$st = st
