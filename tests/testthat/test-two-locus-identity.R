f6cc3935c61de63cf47f9f503765bfd85fbfa3a4
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2019-09-11 12:49:13 +0200

    Finish `twoLocusIdentity()` test in FS/selfing

--- tests/testthat/test-two-locus-identity.R
+++ tests/testthat/test-two-locus-identity.R
@@ -36,18 +36,33 @@ test_that("two-loc-identity agrees with theory in FS with selfing", {
   # Theory
   FSs = function(r) {
     rb = 1-r
+    R = r^2 + rb^2
     M = matrix(0, nrow = 9, ncol = 9, dimnames = rep(list(paste0("D", 1:9)), 2))
     M[1,1] = 1/8*(r^4 + rb^4)
-    M[2,1] = M[1,2] = 1/4*rb^2*r^2
-    M[3,1] = M[1,3] = 1/4*rb*r*(r^2 + rb^2)
-    M[7,1] = M[1,7] = 1/2*rb^2*r^2
+    M[1,2] = M[2,1] = 1/4*rb^2*r^2
+    M[1,3] = M[3,1] = 1/4*rb*r*R
+    M[1,5] = M[5,1] = 1/4*rb*r*R
+    M[1,7] = M[7,1] = 1/2*rb^2*r^2
 
-    M[5,] = M[,5] = M[3,]
-    M[6,] = M[,6] = M[4,]
+    M[2,2] = 1/8*(rb^4 + r^4)
+    M[2,3] = M[3,2] = 1/4*rb*r*R
+    M[2,5] = M[5,2] = 1/4*rb*r*R
+    M[2,7] = M[7,2] = 1/2*rb^2*r^2
+
+    M[3,3] = 1/4*R^2
+    M[3,5] = M[5,3] = rb^2*r^2
+    M[3,7] = M[7,3] = 1/2*rb*r*R
+
+    M[5,5] = 1/4*R^2
+    M[5,7] = M[7,5] = 1/2*rb*r*R
+
+    M[7,7] = 1/4*R^2
+
+    M[4,] = M[,4] = M[6,] = M[,6] = M[8,] = M[,8] = M[9,] = M[,9] = 0
     M
   }
 
-  expect_identical(J2[1, ], FSs(0.25)[1, ]) # TODO: finish this
+  expect_equal(J2, FSs(0.25))
 })
 
 
