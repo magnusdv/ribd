420f77d4c7b64a62a88af4ae670d8a60cefac9af
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2019-09-16 13:52:47 +0200

    Document `jicaque` pedigree

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Jicaque pedigree
+#'
+#' A data frame describing a pedigree from the Jicaque tribe, studied by Chapman
+#' and Jacquard (1971).
+#'
+#' @format A data frame with 22 rows and four columns:
+#'
+#'  * `id`  : individual ID
+#'  * `fid` : father's ID (or 0 if not included)
+#'  * `mid` : mother's ID (or 0 if not included)
+#'  * `sex` : Gender codes, where 1 = male and 2 = female
+#'
+#' @references Chapman, A.M and Jacquard, A. (1971). Un isolat d'Amerique
+#'   Centrale: les Indiens Jicaques de Honduras. In Genetique et Population.
+#'   Paris: Presses Universitaires de France.
+"jicaque"
