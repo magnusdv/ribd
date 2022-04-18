ce0f20236f817585da642b4ef45db67c6249b1f1
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2022-04-18 18:59:56 +0200

    `coeffTable()` gains arguments `identity` and `detailed`, replacing the previous `Delta`.
    * Also rename `coeffs` --> `coeff`

--- R/coeffTable.R
+++ R/coeffTable.R
@@ -3,7 +3,7 @@
 #' Creates a data frame containing various relatedness coefficients between all
 #' pairs of individuals in a given pedigree.
 #'
-#' Available coefficients (indicated in `coeffs`) include:
+#' Available coefficients (indicated in `coeff`) include:
 #'
 #' * f: The inbreeding coefficient of each pair member. Columns: `f1` and `f2`.
 #'
@@ -14,18 +14,25 @@
 #' * kappa: The IBD coefficients computed by [kappaIBD]. (These are NA for pairs
 #' involving inbred individuals.) Columns: `kappa0`, `kappa1`, `kappa2`.
 #'
-#' * Delta: The condensed identity coefficients of Jacquard, computed by
-#' [condensedIdentity()]. Columns: `D1`, ..., `D9`.
+#' * identity: The 9 condensed identity coefficients of Jacquard, computed by
+#' [identityCoefs()]. Columns: `D1`, ..., `D9`.
+#'
+#' * detailed: The detailed identity coefficients of Jacquard, computed by
+#' `identityCoefs(..., detailed = TRUE)`. Columns: `d1`, ..., `d15`.
 #'
 #' @param x A pedigree in the form of a pedtools::ped object.
 #' @param ids A character (or coercible to character) containing ID labels of
 #'   two or more pedigree members.
-#' @param coeffs A character vector containing one or more of the keywords "f",
-#'   "phi", "deg", "kappa", "Delta".
-#'
-#' @return A data frame.
+#' @param coeff A character vector containing one or more of the keywords "f",
+#'   "phi", "deg", "kappa", "identity", "detailed".
 #' @param Xchrom A logical indicating if the coefficients should be autosomal
 #'   (default) or X-chromosomal. If `Xchrom = NA`, both sets are included.
+#' @param self A logical.
+
+#' @return A data frame with one row for each pair of individuals. The first two
+#'   columns are characters named `id1` and `id2`, while remaining columns are
+#'   numeric. For the columnms containing X-chromosomal coefficients, names are
+#'   prefixed with "X-".
 #'
 #' @examples
 #' # Uncle-nephew pedigree
@@ -35,13 +42,22 @@
 #' coeffTable(x)
 #'
 #' # Only relevant coefficients
-#' coeffTable(x, coeffs = c("phi", "deg", "kappa"))
+#' coeffTable(x, coeff = c("phi", "deg", "kappa"))
 #'
 #' # Only the uncle-nephew pair
-#' coeffTable(x, ids = c(3, 6), coeffs = c("phi", "deg", "kappa"))
+#' coeffTable(x, ids = c(3, 6), coeff = c("phi", "deg", "kappa"))
+#'
+#' # X-chromosomal coefficients
+#' coeffTable(x, Xchrom = TRUE)
+#'
+#' # Both autosomal and X
+#' coeffTable(x, Xchrom = NA)
+#'
 #' @export
-coeffTable = function(x, ids = labels(x), coeffs = c("f", "phi", "deg", "kappa", "Delta")) {
-  coeffs = match.arg(coeffs, several.ok = TRUE)
+coeffTable = function(x, ids = labels(x), coeff = c("f", "phi", "deg", "kappa", "identity", "detailed"),
+                      Xchrom = FALSE, self = FALSE) {
+
+  coeff = match.arg(coeff, several.ok = TRUE)
 
   # Xchrom = NA --> both
   if(is.na(Xchrom)) {
@@ -62,7 +78,7 @@ coeffTable = function(x, ids = labels(x), coeffs = c("f", "phi", "deg", "kappa",
     res = cbind(res, f1 = inb[id1], f2 = inb[id2])
   }
 
-  if("phi" %in% coeff || "deg" %in% coeffs) {
+  if("phi" %in% coeff || "deg" %in% coeff) {
     phiMat = kinship(x, Xchrom = Xchrom)
     phi = phiMat[cbind(id1, id2)]
   }
@@ -79,11 +95,15 @@ coeffTable = function(x, ids = labels(x), coeffs = c("f", "phi", "deg", "kappa",
     res = cbind(res, kappa[, -(1:2)])
   }
 
-  if("Delta" %in% coeffs) {
+  if("identity" %in% coeff) {
     delta = identityCoefs(x, ids = ids, detailed = FALSE, simplify = FALSE, Xchrom = Xchrom, verbose = FALSE)
     res = cbind(res, delta[, -(1:2)])
   }
 
+  if("detailed" %in% coeff) {
+    delta = identityCoefs(x, ids = ids, detailed = TRUE, simplify = FALSE, Xchrom = Xchrom, verbose = FALSE)
+    res = cbind(res, delta[, -(1:2)])
+  }
 
   if(Xchrom)
     names(res)[-(1:2)] = paste0("X-", names(res)[-(1:2)])
