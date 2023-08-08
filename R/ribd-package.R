eee07a503f04569bb1fb4e3b6fe2e2f571eefef6
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2023-08-08 09:25:38 +0200

    Update package doc

--- R/ribd-package.R
+++ R/ribd-package.R
@@ -1,12 +1,6 @@
-#' ribd: Computation of pedigree-based relatedness coefficients
-#'
-#' Recursive algorithms for computing various relatedness coefficients,
-#' including Jacquard's condensed identity coefficients. The standard algorithms
-#' are extended to allow inbred founders. Both autosomal and X-linked
-#' coefficients are computed.
-#'
-#' @docType package
-#' @import pedtools
-#'
-#' @name ribd
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
 NULL
